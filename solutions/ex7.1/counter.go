e065e60ec17c91d6352c7c1774e415ac8cb8d56b
aliml92 <almowins5yearsss@gmail.com>
2022-10-18 10:29:13 +0900

    add test

--- solutions/ex7.1/counter.go
+++ solutions/ex7.1/counter.go
@@ -1,68 +1,33 @@
-package main
+package ex71
 
 import (
-	// "bufio"
 	"bufio"
-	"fmt"
-	"io"
-	"log"
-	"os"
-	"text/scanner"
+	"bytes"
 )
 
 
-type LineCounter struct {
-	lines int
+type LineCounter int
+
+func (c *LineCounter) Read(p []byte) (int, error) {
+	scanner := bufio.NewScanner(bytes.NewReader(p))
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		*c++
+	}
+	return len(p), nil
 }
 
-func (c *LineCounter) Write(p []byte) (n int, err error) {
-	for _, b := range p {
-		if b == '\n' {
-			c.lines++
-		}
+
+type WordCounter int
+
+func (c *WordCounter) Read(p []byte) (int, error) {
+	scanner := bufio.NewScanner(bytes.NewReader(p))
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		*c++
 	}
 	return len(p), nil
 }
 
 
 
-
-
-func main(){
-	f, err := os.Open("sample.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-
-	b, err := io.ReadAll(f)
-	if err != nil {
-		log.Fatal(err)
-	}
-	c := &LineCounter{}
-	c.Write(b)
-	fmt.Printf("Line count is: %d\n", c.lines)
-	
-	scanner := bufio.NewScanner(f)
-	scanner.Split(bufio.ScanWords)
-
-	// count := 0
-	// for scanner.Scan() {
-	// 	count++
-	// }
-	// if err := scanner.Err(); err != nil {
-	// 	fmt.Fprintln(os.Stderr, "reading standard input:", err)
-	// }
-	// fmt.Printf("%d\n", count)
-	// scanner.Split(bufio.ScanLines)
-	// // Count the words.
-	// count := 0
-	// for scanner.Scan() {
-	// 	count++
-	// 	// fmt.Println(scanner.Text())
-	// }
-	// if err := scanner.Err(); err != nil {
-	// 	fmt.Fprintln(os.Stderr, "reading input:", err)
-	// }
-	// fmt.Printf("%d\n", count)
-}
---
+++ solutions/ex7.1/counter_test.go
@@ -0,0 +1,56 @@
+package ex71
+
+import (
+	"testing"
+)
+
+
+
+func TestLineCounter(t *testing.T){
+	tests := []struct{
+		name string
+		input []byte
+		want  int
+	}{
+		{name: "empty", input: []byte(""), want: 0},
+		{name: "simple", input: []byte("first line\nsecond line\nthird line\n"), want: 3},
+	}
+	for _, tc := range tests {
+		var c LineCounter
+		n, err := c.Read(tc.input)
+		if n != len(tc.input) {
+			t.Errorf("input length: %d != %d ", len(tc.input), n)
+		}
+		if err != nil {
+			t.Errorf("err: %v", err)
+		}
+		if tc.want != int(c) {
+			t.Fatalf("%s: expected: %v, got: %v", tc.name, tc.want, c)
+		}
+	}
+}
+
+
+func TestWordCounter(t *testing.T){
+	tests := []struct{
+		name string
+		input []byte
+		want  int
+	}{
+		{name: "empty", input: []byte(""), want: 0},
+		{name: "simple", input: []byte("first line\nsecond line\nthird line\n"), want: 6},
+	}
+	for _, tc := range tests {
+		var c WordCounter
+		n, err := c.Read(tc.input)
+		if n != len(tc.input) {
+			t.Errorf("input length: %d != %d ", len(tc.input), n)
+		}
+		if err != nil {
+			t.Errorf("err: %v", err)
+		}
+		if tc.want != int(c) {
+			t.Fatalf("%s: expected: %v, got: %v", tc.name, tc.want, c)
+		}
+	}
+}
